cb60f73fd9153859a20cc15fc27b950e7520ce27
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-08-31 16:38:36 -1000

    completes denormal_cars.sql queries

---
+++ denormal_cars.sql
@@ -0,0 +1,16 @@
+SELECT make_title
+FROM car_models
+GROUP BY make_title;
+
+SELECT model_title
+FROM car_models
+WHERE make_code = 'VOLKS'
+GROUP BY model_title;
+
+SELECT make_code, model_code, model_title, year
+FROM car_models
+WHERE make_code = 'LAM';
+
+SELECT *
+FROM car_models
+WHERE year BETWEEN 2010 AND 2015;
